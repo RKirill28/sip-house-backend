ad8cbef25e0d0150b733741ab5db71e69c127776
RKirill28 <pavlov26.2008@gmail.com>
2026-01-15 05:09:25 +0700

    added done projects get all router

--- src/api/v1/done_projects.py
+++ src/api/v1/done_projects.py
@@ -1,29 +1,22 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter
 
 from src.core.conifg import settings
-from src.core.enums import ProjectSortBy
-from src.core.schemas import CreateProjectModel, ReadProjectModel, UpdateProjectModel
 
-from src.api.deps import DoneProjectRepository
+from src.api.deps import AllParamsDap, DoneProjectRepoDap
 from src.core.schemas import (
     ReadDoneProjectModel,
     ReadAllDoneProjectsModel,
-    CreateDoneProjectModel,
 )
 
 
 done_projects_router = APIRouter(prefix=settings.api.v1.done_projects_prefix)
 
-# @done_projects_router.get("/", response_model=ReadAllProjectsModel)
-# async def get_all_projects(
-#     project_repo: ProjectRepoDap,
-#     offset: int = Query(0),
-#     limit: int = Query(10),
-#     sort_by: ProjectSortBy = Query(),
-#     is_desc: bool = False,
-# ):
-#     projects, count = await project_repo.get_all(offset, limit, sort_by, is_desc)
-#     items = [ReadProjectModel.model_validate(p) for p in projects]
-#
-#     return ReadAllProjectsModel(items=items, count=count)
-#
+
+@done_projects_router.get("/", response_model=ReadAllDoneProjectsModel)
+async def get_all_projects(project_repo: DoneProjectRepoDap, params: AllParamsDap):
+    projects, count = await project_repo.get_all(
+        params["offset"], params["limit"], params["sort_by"], params["is_desc"]
+    )
+    items = [ReadDoneProjectModel.model_validate(p) for p in projects]
+
+    return ReadAllDoneProjectsModel(items=items, count=count)
