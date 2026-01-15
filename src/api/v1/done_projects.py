56f8187c6ce21e54bb09f8ac02de0893fab7a0d6
RKirill28 <pavlov26.2008@gmail.com>
2026-01-15 17:35:39 +0700

    added create done project router

--- src/api/v1/done_projects.py
+++ src/api/v1/done_projects.py
@@ -7,12 +7,22 @@ from src.core.schemas import (
     ReadDoneProjectModel,
     ReadAllDoneProjectsModel,
 )
+from src.core.schemas.done_project import CreateDoneProjectModel
 
 
 done_projects_router = APIRouter(prefix=settings.api.v1.done_projects_prefix)
 
 
-@done_projects_router.get("/", response_model=ReadAllDoneProjectsModel)
+@done_projects_router.post("", response_model=ReadDoneProjectModel)
+async def create_project(
+    project_repo: DoneProjectRepoDap, create_project: CreateDoneProjectModel
+):
+    new = await project_repo.create(create_project)
+    await project_repo.session.commit()
+    return new
+
+
+@done_projects_router.get("", response_model=ReadAllDoneProjectsModel)
 async def get_all_projects(project_repo: DoneProjectRepoDap, params: AllParamsDap):
     projects, count = await project_repo.get_all(
         params["offset"], params["limit"], params["sort_by"], params["is_desc"]
