de46d9c11a5fbdf8d88b41a82dd0d8f290b135c2
RKirill28 <pavlov26.2008@gmail.com>
2026-01-23 12:18:38 +0700

    added admins table

--- src/core/db/models/admin.py
+++ src/core/db/models/admin.py
@@ -1,1 +1,16 @@
+from .base import Base
 
+from datetime import datetime
+from uuid import UUID
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Admin(Base):
+    __tablename__ = "admins"
+
+    username: Mapped[str] = mapped_column(String(32), unique=True, index=True)
+    password: Mapped[str] = mapped_column(String(32))
+    refresh_token: Mapped[UUID | None] = mapped_column(unique=True, default=None)
+    expires_token: Mapped[datetime | None] = mapped_column(default=None)
