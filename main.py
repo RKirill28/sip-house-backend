1594ffeb2521736990a11324e45d35bbf2d5760e
RKirill28 <pavlov26.2008@gmail.com>
2026-01-31 18:37:52 +0700

    added cors for localhost

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 
 from contextlib import asynccontextmanager
 
+from starlette.middleware.cors import CORSMiddleware
+
 from src.core.conifg import settings
 from src.core.db.helper import create_all
 from src.api import main_router
@@ -21,6 +23,19 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan, docs_url="/docs_", redoc_url=None)
 app.include_router(main_router)
+origins = [
+    "http://localhost",
+    "http://localhost:4200",
+    "http://localhost:4201",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
